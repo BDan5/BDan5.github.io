0d856abc34c6710414d5463343e71052ec0d3632
BRaeDon <50760968+BDan5@users.noreply.github.com>
2023-05-17 13:53:05 -0700

    Update rows.js

--- rows.js
+++ rows.js
@@ -1,21 +1,8 @@
 const rows = document.querySelectorAll('.row');
 const colors = ['green', 'teal', 'yellow', 'orange', 'orangered', 'red'];
 
-const onDragOver = (event) => {
-  event.preventDefault();
-}
-
-const onDrop = (event) => {
-  event.preventDefault();
-  const draggedCardId = event.dataTransfer.getData('id');
-  const draggedCard = document.getElementById(draggedCardId);
-  event.target.appendChild(draggedCard);
-  console.log('dropped the element');
-}
 
 rows.forEach((row, index) => {
   const label = row.querySelector('.label');
   label.style.backgroundColor = colors[index];
-  row.ondragover = onDragOver;
-  row.ondrop = onDrop;
 })
